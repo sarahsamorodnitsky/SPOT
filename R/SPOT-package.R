2648a67f998cb9692ddfd9b9e259aae98fcab466
Sarah Samorodnitsky <35846144+sarahsamorodnitsky@users.noreply.github.com>
2024-05-16 10:09:28 -0500

    Update dependencies

---
+++ R/SPOT-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom spatstat.explore Kcross
+#' @importFrom spatstat.explore Kest
+#' @importFrom spatstat.explore Kinhom
+#' @importFrom spatstat.explore Lcross
+#' @importFrom spatstat.explore Lest
+#' @importFrom spatstat.explore Linhom
+#' @importFrom spatstat.geom convexhull.xy
+#' @importFrom stats binomial
+#' @importFrom stats glm
+#' @importFrom stats lm
+#' @importFrom stats reformulate
+## usethis namespace: end
+NULL
